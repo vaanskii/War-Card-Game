2eb13785232b414c84deea3a3b9dc033b23d69e2
vaanskii <giorgivanadze03@gmail.com>
2024-07-19 19:16:32 +0400

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Giorgi Vanadze on 19.07.24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
